04ba59a02b0d10d95053ca748fd9e7ef6e9adc7c
ThiagoTaiga1 <68219703+ThiagoTaiga1@users.noreply.github.com>
2021-09-19 11:43:41 -0300

    Jc Company
    Commit marcação do sistema

---
+++ Painel_JC/index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Painel Controle JC </title>
+    <link rel="stylesheet" href="estilo/fontawesome.min.css" /> <!--load all styles -->
+    <link rel = "preconnect" href = "https://fonts.googleapis.com">
+    <link rel = "preconnect" href = "https://fonts.gstatic.com" crossorigin>
+    <link href = "https: //fonts.googleapis.com/css2? family = Abrir + Sans: wght @ 300 & display = swap "rel =" stylesheet ">
+    <link rel="stylesheet" href="estilo/style.css">
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keyword" content="´plavaras-chave, do , meu ,site">
+    <meta name="description" content="Descrição do meu site">
+</head>
+<body>
+
+    <header>
+        <div class="center">
+        <div class="logo left">Logomarca</div><!--logo-->
+        <nav class="desktop rigth">
+    <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Sobre</a></li>
+        <li><a href="">Serviços</a></li>
+        <li><a href="">Contato</a></li>
+    </ul>
+    </nav>
+    <nav class="mobile left">
+    <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Sobre</a></li>
+        <li><a href="">Serviços</a></li>
+        <li><a href="">Contato</a></li>
+    </ul>
+    </nav>
+    <div class="clear"></div>
+    </div><!--center-->
+    </header>
+
+<section class="banner-principal">
+<form>
+    <div class="center">
+    <h2>
+    <input type="email" name="email" required/>
+    <input type="submit" name="acao" required/>
+    </h2>
+
+</form>
+</div><!--center-->
+</section><!--banner-principal-->
+
+        <section class="descricao-autor">
+        <div class="center">
+        <div class="w50 left">
+        <h2>Journey Company</h2>
+        <p>Lorem Ipsum é simplesmente um texto fictício da indústria de impressão e composição. Lorem Ipsum tem sido o texto fictício padrão da indústria desde os anos 1500, quando um impressor desconhecido pegou uma galé do tipo e embaralhou para fazer um livro de amostra de tipos. Ele sobreviveu não apenas cinco séculos, mas também ao salto para a composição eletrônica, permanecendo essencialmente inalterado. Ele foi popularizado na década de 1960 com o lançamento de folhas de Letraset contendo passagens de Lorem Ipsum e, mais recentemente, com software de editoração eletrônica como Aldus PageMaker incluindo versões de Lorem Ipsum.</p>
+        <p>Lorem Ipsum é simplesmente um texto fictício da indústria de impressão e composição. Lorem Ipsum tem sido o texto fictício padrão da indústria desde os anos 1500, quando um impressor desconhecido pegou uma galé do tipo e embaralhou para fazer um livro de amostra de tipos. Ele sobreviveu não apenas cinco séculos, mas também ao salto para a composição eletrônica, permanecendo essencialmente inalterado. Ele foi popularizado na década de 1960 com o lançamento de folhas de Letraset contendo passagens de Lorem Ipsum e, mais recentemente, com software de editoração eletrônica como Aldus PageMaker incluindo versões de Lorem Ipsum.</p>
+
+        </div><!--w50-->
+    <div class="w50 left">
+    <img src="imagens/foto.jpg">
+    </div><!--w50-->
+    <div class="clear"></div>
+    </div><!--center-->
+    </section><!--descricao-autor-->
+   
+    <section class="especialidades">
+    <div class="center">
+    <h2 class="title">Especialidades</h2>
+        <div class="w33 left box-especialidades">
+    
+       <h3> <i class="fas fa-mobile" aria-hidden="true"></i></h3>
+        <h3>icone1</h3>
+        <h3>Sistemas</h3>
+        <p>Lorem Ipsum é simplesmente um texto fictício da indústria de impressão e composição. Lorem Ipsum tem sido o texto fictício padrão da indústria desde os anos 1500, quando um impressor desconhecido pegou uma galé do tipo e embaralhou para fazer um livro de amostra de tipos. Ele sobreviveu não apenas cinco séculos, mas também ao salto para a composição eletrônica, permanecendo essencialmente inalterado. Ele foi popularizado na década de 1960 com o lançamento de folhas de Letraset contendo passagens de Lorem Ipsum e, mais recentemente, com software de editoração eletrônica como Aldus PageMaker incluindo versões de Lorem Ipsum.</p>
+        </div><!--especialidades-->
+        <div class="w33 left box-especialidades">
+        <i class="fas fa-mobile"></i>
+        <h3>icone2</h3>
+        <h3>Web</h3>
+        <p>Lorem Ipsum é simplesmente um texto fictício da indústria de impressão e composição. Lorem Ipsum tem sido o texto fictício padrão da indústria desde os anos 1500, quando um impressor desconhecido pegou uma galé do tipo e embaralhou para fazer um livro de amostra de tipos. Ele sobreviveu não apenas cinco séculos, mas também ao salto para a composição eletrônica, permanecendo essencialmente inalterado. Ele foi popularizado na década de 1960 com o lançamento de folhas de Letraset contendo passagens de Lorem Ipsum e, mais recentemente, com software de editoração eletrônica como Aldus PageMaker incluindo versões de Lorem Ipsum.</p>
+        </div><!--especialidades-->
+        <div class="w33 left box-especialidades">
+        <i class="fas fa-mobile"></i>
+        <h3>icone3</h3>
+        <h3>App</h3>
+        <p>Lorem Ipsum é simplesmente um texto fictício da indústria de impressão e composição. Lorem Ipsum tem sido o texto fictício padrão da indústria desde os anos 1500, quando um impressor desconhecido pegou uma galé do tipo e embaralhou para fazer um livro de amostra de tipos. Ele sobreviveu não apenas cinco séculos, mas também ao salto para a composição eletrônica, permanecendo essencialmente inalterado. Ele foi popularizado na década de 1960 com o lançamento de folhas de Letraset contendo passagens de Lorem Ipsum e, mais recentemente, com software de editoração eletrônica como Aldus PageMaker incluindo versões de Lorem Ipsum.</p>
+        </div><!--especialidades-->
+    </div><!--center-->
+    </section><!--especialidades-->
+        <section class="extras">
+            <section class="center">
+                <div class="w50 left"></div>
+                <h2 class="title">Depoimentos</h2>
+                <div class="depoimentp-single">
+                <p>Lorem Ipsum é simplesmente um texto fictício da indústria de impressão e composição. Lorem Ipsum tem sido o texto fictício padrão da indústria desde os anos 1500, quando um impressor desconhecido pegou uma galé do tipo e embaralhou para fazer um livro de amostra de tipos. Ele sobreviveu não apenas cinco séculos, mas também ao salto para a composição eletrônica, permanecendo essencialmente inalterado. Ele foi popularizado na década de 1960 com o lançamento de folhas de Letraset contendo passagens de Lorem Ipsum e, mais recentemente, com software de editoração eletrônica como Aldus PageMaker incluindo versões de Lorem Ipsum.</p>
+                <p class="nome-autor">Lorem Ipsulum</p>
+                </div><!--depoimento-single-->
+                <div class="depoimentp-single">
+                <p>Lorem Ipsum é simplesmente um texto fictício da indústria de impressão e composição. Lorem Ipsum tem sido o texto fictício padrão da indústria desde os anos 1500, quando um impressor desconhecido pegou uma galé do tipo e embaralhou para fazer um livro de amostra de tipos. Ele sobreviveu não apenas cinco séculos, mas também ao salto para a composição eletrônica, permanecendo essencialmente inalterado. Ele foi popularizado na década de 1960 com o lançamento de folhas de Letraset contendo passagens de Lorem Ipsum e, mais recentemente, com software de editoração eletrônica como Aldus PageMaker incluindo versões de Lorem Ipsum.</p>
+                <p class="nome-autor">Lorem Ipsulum</p>
+
+                </div><!--depoimento-single-->
+                <div class="depoimentp-single">
+                <p>Lorem Ipsum é simplesmente um texto fictício da indústria de impressão e composição. Lorem Ipsum tem sido o texto fictício padrão da indústria desde os anos 1500, quando um impressor desconhecido pegou uma galé do tipo e embaralhou para fazer um livro de amostra de tipos. Ele sobreviveu não apenas cinco séculos, mas também ao salto para a composição eletrônica, permanecendo essencialmente inalterado. Ele foi popularizado na década de 1960 com o lançamento de folhas de Letraset contendo passagens de Lorem Ipsum e, mais recentemente, com software de editoração eletrônica como Aldus PageMaker incluindo versões de Lorem Ipsum.</p>
+                <p class="nome-autor">Lorem Ipsulum</p>
+
+                </div><!--depoimento-single-->
+             
+                </div><!--w50-->
+
+                <div class="w50 left"></div>
+                <h2 class="title">Serviços</h2>
+                <div class="servicos">
+                <ul>
+
+                <li>Lorem Ipsum é simplesmente um texto fictício da indústria de impressão e composição. Lorem Ipsum tem sido o texto fictício padrão da indústria desde os anos 1500, quando um impressor desconhecido pegou uma galé do tipo e embaralhou para fazer um livro de amostra de tipos. Ele sobreviveu não apenas cinco séculos, mas também ao salto para a composição eletrônica, permanecendo essencialmente inalterado. Ele foi popularizado na década de 1960 com o lançamento de folhas de Letraset contendo passagens de Lorem Ipsum e, mais recentemente, com software de editoração eletrônica como Aldus PageMaker incluindo versões de Lorem Ipsum.
+                </li>
+                <li>Lorem Ipsum é simplesmente um texto fictício da indústria de impressão e composição. Lorem Ipsum tem sido o texto fictício padrão da indústria desde os anos 1500, quando um impressor desconhecido pegou uma galé do tipo e embaralhou para fazer um livro de amostra de tipos. Ele sobreviveu não apenas cinco séculos, mas também ao salto para a composição eletrônica, permanecendo essencialmente inalterado. Ele foi popularizado na década de 1960 com o lançamento de folhas de Letraset contendo passagens de Lorem Ipsum e, mais recentemente, com software de editoração eletrônica como Aldus PageMaker incluindo versões de Lorem Ipsum.
+                </li>
+                <li>Lorem Ipsum é simplesmente um texto fictício da indústria de impressão e composição. Lorem Ipsum tem sido o texto fictício padrão da indústria desde os anos 1500, quando um impressor desconhecido pegou uma galé do tipo e embaralhou para fazer um livro de amostra de tipos. Ele sobreviveu não apenas cinco séculos, mas também ao salto para a composição eletrônica, permanecendo essencialmente inalterado. Ele foi popularizado na década de 1960 com o lançamento de folhas de Letraset contendo passagens de Lorem Ipsum e, mais recentemente, com software de editoração eletrônica como Aldus PageMaker incluindo versões de Lorem Ipsum.
+                </li>
+                </ul>
+                </div><!--servicos-->
+                
+                </div><!--w50-->
+                <div class="clear"></div>
+                    </section><!--center-->
+                        </section><!--extras-->
+
+                        <footer>
+                            <p>Todos os direitos Reservado</p>
+                        </footer>
+</body>
+</html> 
